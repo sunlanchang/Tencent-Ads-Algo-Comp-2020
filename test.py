739f28e251c12cfcb1f0a2836dbd79b5c94a69d8
sunlanchang <sunlanchang@outlook.com>
2020-05-14 18:05:05 +0800

    修改多进程

--- test.py
+++ test.py
@@ -1,36 +1,12 @@
-from multiprocessing import Process
-import multiprocessing
+# %%
 import pandas as pd
-# 子进程要执行的代码
-from multiprocessing import Pool, cpu_count
 import os
-import time
-
-# print(multiprocessing.cpu_count())
-df_creativeid_embedding = pd.read_hdf(
-    'word2vec/df_creativeid_embedding.h5',
-    key='df_creativeid_embedding', mode='r')
-
-with open('word2vec/userid_creativeids.txt', 'r')as f:
-    seq_creative_id = f.readlines()
-seq_creative_id = [[str(e) for e in line.strip().split(' ')]
-                   for line in seq_creative_id]
+# %%
+user_embeddings = []
+for i in range(os.cpu_count()):
+    tmp = pd.read_hdf(
+        '/tmp/df_user_embedding{}.h5'.format(i), key='df_user_embedding{}'.format(i), mode='r')
+    user_embeddings.append(tmp)
 
 
-def long_time_task(i):
-    print('子进程: {} - 任务{}'.format(os.getpid(), i))
-    print(df_creativeid_embedding.iloc[0, 0])
-
-
-if __name__ == '__main__':
-    p = Pool(4)
-    indexes = [(0, 1000000), (1000000, 1900000)]
-    for index in indexes:
-        p.apply_async(long_time_task, args=(index,))
-    print('等待所有子进程完成。')
-    p.close()
-    p.join()
-
-
-# for user in tqdm.tqdm(range(len(seq_creative_id))):
-#     user_em = df_creativeid_embedding.loc[seq_creative_id[user]].mean()
+# %%
