fe6be84937a24807e85e87437341ce6c68c45033
sunlanchang <sunlanchang@outlook.com>
2020-06-04 18:18:30 +0800

    update lstm

--- LSTM_age.py
+++ LSTM_age.py
@@ -156,7 +156,7 @@ else:
     sequences = tokenizer.texts_to_sequences(
         creative_id_seq[900000:])
 
-X_test = pad_sequences(sequences, maxlen=max_len_creative_id, padding='pre')
+X_test = pad_sequences(sequences, maxlen=max_len_creative_id, padding='post')
 
 
 # %%
