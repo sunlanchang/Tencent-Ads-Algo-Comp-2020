4a0826fd0de33271e9074af60dfef98e07616e88
sunlanchang <sunlanchang@outlook.com>
2020-06-07 11:14:05 +0800

    update multi input lstm

--- LSTM_gender_multi_input.py
+++ LSTM_gender_multi_input.py
@@ -12,7 +12,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 from mymail import mail
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 # %%
 # f = open('word2vec/userid_creative_ids.txt')
@@ -26,6 +26,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 # %%
 NUM_creative_id = 2481135+1
 NUM_ad_id = 2264190+1
+NUM_product_id = 33273+1
 
 
 def get_train_val():
@@ -76,13 +77,6 @@ def get_train_val():
     X2_train = pad_sequences(
         sequences, maxlen=LEN_ad_id, padding='post')
 
-    # 获得gender标签
-    user_train = pd.read_csv(
-        'data/train_preliminary/user.csv').sort_values(['user_id'], ascending=(True,))
-    Y_gender = user_train['gender'].values
-    Y_age = user_train['age'].values
-    Y_gender = Y_gender - 1
-
     def get_ad_id_emb():
         path = "word2vec/wordvectors_ad_id.kv"
         wv = KeyedVectors.load(path, mmap='r')
@@ -99,11 +93,49 @@ def get_train_val():
 
     ad_id_emb = get_ad_id_emb()
 
+    # 获取 product_id 特征
+    # f = open('tmp/userid_product_ids.txt')
+    f = open('word2vec/userid_product_ids.txt')
+    tokenizer = Tokenizer(num_words=NUM_product_id)
+    tokenizer.fit_on_texts(f)
+    f.close()
+    product_id_seq = []
+    with open('word2vec/userid_product_ids.txt') as f:
+        for text in f:
+            product_id_seq.append(text.strip())
+
+    sequences = tokenizer.texts_to_sequences(product_id_seq[:900000//1])
+    X3_train = pad_sequences(
+        sequences, maxlen=LEN_product_id, padding='post')
+
+    # 获取product_id embedding
+    def get_product_id_emb():
+        path = "word2vec/wordvectors_product_id.kv"
+        wv = KeyedVectors.load(path, mmap='r')
+        product_id_tokens = list(wv.vocab.keys())
+        embedding_dim = 128
+        embedding_matrix = np.random.randn(
+            len(product_id_tokens)+1, embedding_dim)
+        for product_id in product_id_tokens:
+            embedding_vector = wv[product_id]
+            if embedding_vector is not None:
+                index = tokenizer.texts_to_sequences([product_id])[0][0]
+                embedding_matrix[index] = embedding_vector
+        return embedding_matrix
+
+    product_id_emb = get_product_id_emb()
+
+    # 获得gender标签
+    user_train = pd.read_csv(
+        'data/train_preliminary/user.csv').sort_values(['user_id'], ascending=(True,))
+    Y_gender = user_train['gender'].values
+    Y_age = user_train['age'].values
+    Y_gender = Y_gender - 1
     num_examples = Y_gender.shape[0]
     train_examples = int(num_examples * 0.9)
 
     # 分别对应 x1_train x1_val x2_train x2_val y_train y_val
-    return X1_train[:train_examples], X1_train[train_examples:], X2_train[:train_examples], X2_train[train_examples:], Y_gender[:train_examples], Y_gender[train_examples:], creative_id_emb, ad_id_emb
+    return X1_train[:train_examples], X1_train[train_examples:], X2_train[:train_examples], X2_train[train_examples:], X3_train[:train_examples], X3_train[train_examples:], Y_gender[:train_examples], Y_gender[train_examples:], creative_id_emb, ad_id_emb, product_id_emb
 
 # %%
 
@@ -112,16 +144,13 @@ def get_test():
     pass
 
 
-def get_embedding():
-    pass
-
-
 # %%
 LEN_creative_id = 100
 LEN_ad_id = 100
+LEN_product_id = 100
 
 
-def get_gender_model(creative_id_emb, ad_id_emb):
+def get_gender_model(creative_id_emb, ad_id_emb, product_id_emb):
     # shape：(sequence长度, )
     # first input
     input_creative_id = Input(shape=(None,), name='creative_id')
@@ -132,7 +161,8 @@ def get_gender_model(creative_id_emb, ad_id_emb):
                    input_length=LEN_creative_id,
                    mask_zero=True)(input_creative_id)
     x1 = LSTM(1024, return_sequences=True)(x1)
-    x1 = LSTM(512, return_sequences=False)(x1)
+    x1 = LSTM(512, return_sequences=True)(x1)
+    x1 = LSTM(256, return_sequences=False)(x1)
 
     # second input
     input_ad_id = Input(shape=(None,), name='ad_id')
@@ -143,15 +173,28 @@ def get_gender_model(creative_id_emb, ad_id_emb):
                    input_length=LEN_ad_id,
                    mask_zero=True)(input_ad_id)
     x2 = LSTM(1024, return_sequences=True)(x2)
-    x2 = LSTM(512, return_sequences=False)(x2)
+    x2 = LSTM(512, return_sequences=True)(x2)
+    x2 = LSTM(256, return_sequences=False)(x2)
+
+    # third input
+    input_product_id = Input(shape=(None,), name='product_id')
+    x3 = Embedding(input_dim=NUM_product_id,
+                   output_dim=128,
+                   weights=[product_id_emb],
+                   trainable=True,
+                   input_length=LEN_product_id,
+                   mask_zero=True)(input_product_id)
+    x3 = LSTM(1024, return_sequences=True)(x3)
+    x3 = LSTM(512, return_sequences=True)(x3)
+    x3 = LSTM(256, return_sequences=False)(x3)
 
     # concat x1 x2
-    x = concatenate([x1, x2])
+    x = concatenate([x1, x2, x3])
     x = Dense(128)(x)
-    x = Dropout(0.5)(x)
+    x = Dropout(0.1)(x)
     output_y = Dense(1, activation='sigmoid')(x)
 
-    model = Model([input_creative_id, input_ad_id], output_y)
+    model = Model([input_creative_id, input_ad_id, input_product_id], output_y)
     model.compile(loss='binary_crossentropy',
                   optimizer='adam', metrics=['accuracy'])
     model.summary()
@@ -160,9 +203,11 @@ def get_gender_model(creative_id_emb, ad_id_emb):
 
 
 # %%
-x1_train, x1_val, x2_train, x2_val, y_train, y_val, creative_id_emb, ad_id_emb = get_train_val()
+mail('start getting train data')
+x1_train, x1_val, x2_train, x2_val, x3_train, x3_val, y_train, y_val, creative_id_emb, ad_id_emb, product_id_emb = get_train_val()
+mail('get train data done.')
 
-model = get_gender_model(creative_id_emb, ad_id_emb)
+model = get_gender_model(creative_id_emb, ad_id_emb, product_id_emb)
 # %%
 # %%
 # 测试数据格式(batch_size, sequence长度)
@@ -172,7 +217,7 @@ model = get_gender_model(creative_id_emb, ad_id_emb)
 
 
 # %%
-checkpoint = ModelCheckpoint("tmp/gender_epoch_{epoch:02d}.hdf5", monitor='val_loss', verbose=0,
+checkpoint = ModelCheckpoint("tmp/gender_epoch_{epoch:02d}.hdf5", monitor='val_loss', verbose=1,
                              save_best_only=False, mode='auto', period=1)
 # %%
 # model.fit(
@@ -188,10 +233,10 @@ checkpoint = ModelCheckpoint("tmp/gender_epoch_{epoch:02d}.hdf5", monitor='val_l
 try:
     mail('start train lstm')
     model.fit(
-        {'creative_id': x1_train, 'ad_id': x2_train},
+        {'creative_id': x1_train, 'ad_id': x2_train, 'product_id': x3_train},
         y_train,
-        validation_data=([x1_val, x2_val], y_val),
-        epochs=5,
+        validation_data=([x1_val, x2_val, x3_val], y_val),
+        epochs=3,
         batch_size=256,
         callbacks=[checkpoint],
     )
