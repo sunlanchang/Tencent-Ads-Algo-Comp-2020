63963aced31782b2c3296df7382273bb50da148a
sunlanchang <sunlanchang@outlook.com>
2020-06-15 22:43:22 +0800

    update LSTM

--- LSTM_gender_multi_input.py
+++ LSTM_gender_multi_input.py
@@ -283,7 +283,7 @@ if not args.load_from_npy:
             np.save(f'tmp/{name}_{i}.npy', data)
 
     inputs = [DATA['X1_train'], DATA['X1_val'], DATA['X2_train'],
-              DATA['X2_val'], DATA['X3_train'], DATA['x3_val']]
+              DATA['X2_val'], DATA['X3_train'], DATA['X3_val']]
     targets = [DATA['Y_train'], DATA['Y_val']]
     embeddings = [DATA['creative_id_emb'],
                   DATA['ad_id_emb'], DATA['product_id_emb']]
