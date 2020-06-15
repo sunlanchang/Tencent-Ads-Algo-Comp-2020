90b50866a7e533b2943008cd25fb9154b1a58f6d
sunlanchang <sunlanchang@outlook.com>
2020-06-15 12:29:43 +0800

    update LSTM

--- LSTM_age_multi_input.py
+++ LSTM_age_multi_input.py
@@ -23,8 +23,9 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 python LSTM_gender_multi_input.py  --epoch 5 --batch_size 256 --num_lstm 3 --examples 810000
 上边这个命令参数中：
 --epoch 一般是1个就收敛2个就过拟合
+--batch_size 根据你的机器调到最大
 --num_lstm 我测试age时候3比较好能到42.4%，5就下降到41了
---example 设置成810000，也就是0.9的训练集，其中剩下的90000作为验证集
+--example 设置成810000，也就是0.9的训练集，其中剩下的90000作为验证集，调试时候可以设置小的数字
 
 第二次训练用下面这条命令，能够使用第一次保存的中间结果，不用重复生成训练数据
 python LSTM_gender_multi_input.py --load_from_npy --epoch 5 --batch_size 256 --num_lstm 3 --examples 10000
